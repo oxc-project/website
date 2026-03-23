bc7810c44187fb8bc00868e689d1ca9815de5709
Connor Shea <connor.james.shea@gmail.com>
2026-03-22 23:06:07 -0600

    Move RuleHowToUse component to markdown plugin.
    Unfortunately this is necessary to get the syntax highlighting working, otherwise we end up needing to do some very jank, fragile workarounds.

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,7 +5,6 @@ import AppBlogPostHeader from "./components/AppBlogPostHeader.vue";
 import AppBlogList from "./components/AppBlogList.vue";
 import RulesTable from "./components/RulesTable.vue";
 import RuleHeader from "./components/RuleHeader.vue";
-import RuleHowToUse from "./components/RuleHowToUse.vue";
 import RuleReferences from "./components/RuleReferences.vue";
 import Alert from "./components/Alert.vue";
 import "./styles.css";
@@ -18,7 +17,6 @@ export default {
     app.component("AppBlogList", AppBlogList);
     app.component("RulesTable", RulesTable);
     app.component("RuleHeader", RuleHeader);
-    app.component("RuleHowToUse", RuleHowToUse);
     app.component("RuleReferences", RuleReferences);
   },
 } satisfies Theme;
