0c6239b578dd58ca3e29163e3140bf27c0154152
Boshen <boshenc@gmail.com>
2026-07-26 14:06:21 +0800

    docs: document Oxc Node

--- .vitepress/config/shared.ts
+++ .vitepress/config/shared.ts
@@ -176,6 +176,7 @@ export const sharedConfig = {
             { text: "Transformer", link: "/docs/guide/usage/transformer" },
             { text: "Minifier", link: "/docs/guide/usage/minifier" },
             { text: "Resolver", link: "/docs/guide/usage/resolver" },
+            { text: "Oxc Node", link: "/docs/guide/usage/oxc-node" },
           ],
         },
         {
